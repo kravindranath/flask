51f44acab1430450696d33a5bca279f823e26ab6
Ravindranath K <ravindranath.k@gmail.com>
2023-04-22 22:28:27 +0530

    first commit

---
+++ tailwind.py
@@ -0,0 +1,8 @@
+import os
+from os.path import exists
+
+if not (exists('tailwind.config.js')):
+    os.system('tailwindcss.3.3.1.exe init')
+
+if (exists('tailwindcss.3.3.1.exe')):
+    os.system('tailwindcss.3.3.1.exe -i app/static/tailwind/3.3.1/input.css -o app/static/tailwind/3.3.1/output.css --watch --minify')
