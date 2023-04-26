a6fb18055b98428408ca3b3ccd12d05a217597c9
Ravindranath K <ravindranath.k@gmail.com>
2023-04-26 23:40:39 +0530

    configure navigations as dynamic

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,22 @@
 from flask import Flask 
 from flask import render_template
 
+main_navigation = {
+    'Home': '/home',
+    'Login': '/home',
+    'Contact us': '/home',
+}
+
+sidebar_navigation = {
+    'Dashboard': '/home',
+    'Kanban': '/home',
+    'Inbox': '/home',
+    'Users': '/home',
+    'Products': '/home',
+    'Sign': '/home',
+    'Sign': '/home',
+}
+
 def create_app():
     app = Flask(__name__)
     
@@ -8,6 +24,8 @@ def create_app():
     def home():
         return render_template(
             "home.html",
+            main_navigation = main_navigation,
+            sidebar_navigation = sidebar_navigation,
         )
 
     return app
